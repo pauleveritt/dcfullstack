a10b387cbe15394c215cb049c8bc088ad0d94c46
Paul Everitt <pauleveritt@me.com>
2017-06-15 17:23:47 -0400

    App Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,22 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
-import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import {AppComponent} from './app.component';
+import {HeroService} from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule {

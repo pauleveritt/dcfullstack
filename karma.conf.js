f4420b43a9a92608ef243909a5a0b976dd39fd3c
Paul Everitt <pauleveritt@me.com>
2017-06-15 14:07:25 -0400

    Headless Chrome.

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -27,7 +28,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false
   });
 };
